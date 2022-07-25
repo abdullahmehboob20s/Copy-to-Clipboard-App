5bfd8dac0c9bd9b4cd10af5ebe34bb5dff7d76f4
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-07-25 17:23:19 +0500

    added functionalities

---
+++ src/components/CopyToClipboardForm/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+import useCopyToClipboard from "hooks/useCopyToClipboard";
+
+const initialValues = {
+  name: "",
+  number: "",
+  reason: "",
+  resolution: "",
+};
+
+function CopyToClipboardForm() {
+  const { copy, copyStatus } = useCopyToClipboard(1000);
+  const [formState, setFormState] = useState(initialValues);
+
+  let formValueChanger = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+
+    console.log("Clicked");
+
+    copy(`Caller: ${formState.name}
+MDN: ${formState.number}
+Problem: ${formState.reason}
+Resolution: ${formState.resolution}`);
+  };
+
+  const clearinputFilds = () => {
+    setFormState(initialValues);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <h1>Notes:</h1>
+        <form onSubmit={formHandler}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              id="name"
+              name="name"
+              onChange={formValueChanger}
+              value={formState.name}
+              disabled={copyStatus === "copied" ? true : false}
+            />
+          </div>
+          <div>
+            <label htmlFor="number">Phone Number:</label>
+            <input
+              type="number"
+              placeholder="Your Phone Number"
+              id="number"
+              name="number"
+              onChange={formValueChanger}
+              value={formState.number}
+              disabled={copyStatus === "copied" ? true : false}
+            />
+          </div>
+          <div>
+            <label htmlFor="reason">Reason For Call:</label>
+            <textarea
+              placeholder="Your Reason"
+              id="reason"
+              name="reason"
+              onChange={formValueChanger}
+              value={formState.reason}
+              disabled={copyStatus === "copied" ? true : false}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="resolution">Resolution:</label>
+            <textarea
+              placeholder="Your Resolution"
+              id="resolution"
+              name="resolution"
+              onChange={formValueChanger}
+              value={formState.resolution}
+              disabled={copyStatus === "copied" ? true : false}
+            ></textarea>
+          </div>
+          <div className={styles.btns}>
+            <button
+              type="submit"
+              className={copyStatus === "copied" ? styles.copied : ""}
+              disabled={copyStatus === "copied" ? true : false}
+            >
+              {copyStatus === "copied" ? "Copied" : "Copy"}
+            </button>
+            <button type="button" onClick={clearinputFilds}>
+              Clear
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CopyToClipboardForm;
---
+++ src/hooks/useCopyToClipboard.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+function useCopyToClipboard(notifyTimeout = 2000) {
+  const [copiedText, setCopiedText] = useState(null);
+  const [copyStatus, setCopyStatus] = useState("inactive");
+
+  const copy = async (text) => {
+    if (!navigator?.clipboard) {
+      console.warn("Clipboard not supported");
+      return false;
+    }
+
+    // Try to save to clipboard then save it in the state if worked
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopiedText(text);
+      setCopyStatus("copied");
+      return true;
+    } catch (error) {
+      console.warn("Copy failed", error);
+      setCopiedText(null);
+      setCopyStatus("failed");
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    if (copyStatus === "inactive") {
+      return;
+    }
+
+    const timeoutId = setTimeout(
+      () => setCopyStatus("inactive"),
+      notifyTimeout
+    );
+
+    return () => clearTimeout(timeoutId);
+  }, [copyStatus]);
+
+  return { copiedText, copy, copyStatus };
+}
+
+export default useCopyToClipboard;
